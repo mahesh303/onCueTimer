0192697f954553fae8ae8639d9b293cd4c9c71e3
Mahesh Patel <mahesh303@hotmail.com>
2025-01-29 13:12:16 -0500

    Initial Commit

---
+++ onCueTimer/onCueTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  onCueTimerApp.swift
+//  onCueTimer
+//
+//  Created by Mahesh Patel on 1/29/25.
+//
+
+import SwiftUI
+
+@main
+struct onCueTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ onCueTimerTests/onCueTimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  onCueTimerTests.swift
+//  onCueTimerTests
+//
+//  Created by Mahesh Patel on 1/29/25.
+//
+
+import Testing
+@testable import onCueTimer
+
+struct onCueTimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
